7a07e90502cc23679bdf2738be9a36f8239c8e1f
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-12-13 18:31:27 +0100

    Use Regex Validation In React Hook Form And Signup

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -2,41 +2,60 @@ import { createContext, useEffect, useState } from "react";
 import { app } from "../firebase/firebase.config";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
-export const AuthContext = createContext(null);
+export const AuthContext = createContext();
+
 const auth = getAuth(app);
+
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
-    const createUser = (email, password) => {
-        setLoading(true);
-        return createUserWithEmailAndPassword(email, password);
+    const createUser = async (email, password) => {
+        try {
+            setLoading(true);
+            const result = await createUserWithEmailAndPassword(auth, email, password);
+            return result;
+        } catch (error) {
+            console.error("Error creating user:", error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
-    }
+    const signIn = async (email, password) => {
+        try {
+            setLoading(true);
+            const result = await signInWithEmailAndPassword(auth, email, password);
+            return result;
+        } catch (error) {
+            console.error("Error signing in:", error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
-    const signIn = (email, password) => {
-        setLoading(true);
-        return signInWithEmailAndPassword(auth, email, password)
-    }
-
-    const logOut = () => {
-        setLoading(true);
-        return signOut(auth)
-    }
-
+    const logOut = async () => {
+        try {
+            setLoading(true);
+            await signOut(auth);
+        } catch (error) {
+            console.error("Error signing out:", error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser);
             console.log('Current user', currentUser);
             setLoading(false);
-        })
+        });
+
         return () => {
             return unsubscribe();
-        }
-    }, [])
-
-
+        };
+    }, []);
 
     const authInfo = {
         user,
@@ -44,8 +63,8 @@ const AuthProvider = ({ children }) => {
         createUser,
         signIn,
         logOut,
+    };
 
-    }
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
@@ -54,4 +73,3 @@ const AuthProvider = ({ children }) => {
 };
 
 export default AuthProvider;
-
