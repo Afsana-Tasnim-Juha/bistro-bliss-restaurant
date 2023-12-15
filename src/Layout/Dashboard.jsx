bf3374ec6a9aa7ff235b85638b64b3d5874e7585
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-12-15 16:56:54 +0100

    Dashboard navigation

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,5 +1,10 @@
+import { MdShoppingCart } from "react-icons/md";
 import { NavLink, Outlet } from "react-router-dom";
-
+import { IoHome } from "react-icons/io5";
+import { MdOutlineDateRange } from "react-icons/md";
+import { MdOutlinePayment } from "react-icons/md";
+import { MdOutlineRateReview } from "react-icons/md";
+import { TbBrandBooking } from "react-icons/tb";
 
 const Dashboard = () => {
     return (
@@ -7,10 +12,43 @@ const Dashboard = () => {
             {/**
              * Deshboard side bar
              */}
-            <div className="w-64 min-h-full bg-[#D1A054B2]">
-                <ul className="menu">
+            <div className="w-64 min-h-screen bg-[#D1A054B2]">
+                <ul className="menu p-4">
                     <li>
-                        <NavLink to="/dashboard/cart">My Cart</NavLink>
+
+                        <NavLink to="/dashboard/userHome">
+                            <IoHome />
+                            User Home</NavLink>
+                    </li>
+                    <li>
+
+                        <NavLink to="/dashboard/reservation">
+                            <MdOutlineDateRange />
+                            Reservation</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard/payment">
+                            <MdOutlinePayment />
+                            Payment History</NavLink>
+                    </li>
+
+                    <li>
+
+                        <NavLink to="/dashboard/cart">
+                            <MdShoppingCart></MdShoppingCart>
+                            My Cart</NavLink>
+                    </li>
+                    <li>
+
+                        <NavLink to="/dashboard/review">
+                            <MdOutlineRateReview />
+                            Add Review</NavLink>
+                    </li>
+                    <li>
+
+                        <NavLink to="/dashboard/bookings">
+                            <TbBrandBooking />
+                            My Bookings</NavLink>
                     </li>
                 </ul>
             </div>
