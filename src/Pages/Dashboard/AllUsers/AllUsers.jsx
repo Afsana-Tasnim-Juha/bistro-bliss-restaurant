aa524909b5647a1fbe3e962f265cb130ef7e25ce
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2024-02-05 22:48:29 +0100

    Create Make Admin API

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -2,18 +2,50 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from './../../../hooks/useAxiosSecure';
 import { MdDelete } from "react-icons/md";
 import { FaUsers } from 'react-icons/fa';
+import Swal from "sweetalert2";
 
 
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure();
-    const { data: users = [] } = useQuery({
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('/users')
             return res.data;
         },
     })
+
+    const handleMakeAdmin = user => {
+
+    }
+
     const handleDeleteUser = user => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+
+                axiosSecure.delete(`/users/${user._id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+
+                        }
+                    })
+            }
+        });
 
     }
 
@@ -44,7 +76,7 @@ const AllUsers = () => {
                                 <td>{user.email}</td>
                                 <td>
                                     <button
-                                        onClick={() => { handleDeleteUser(user) }}
+                                        onClick={() => { handleMakeAdmin(user) }}
 
                                         className="btn  btn-lg bg-[#BB8506]">
                                         <FaUsers className="text-white text-2xl" />
