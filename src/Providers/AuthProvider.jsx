7e63149243c15f33f45312deb564fa3c5b17e784
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2024-02-06 11:47:55 +0100

    create a jwt token

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { app } from "../firebase/firebase.config";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
+import useAxiosPublic from './../hooks/useAxiosPublic';
 export const AuthContext = createContext();
 
 const auth = getAuth(app);
@@ -10,6 +11,7 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic();
 
     const createUser = async (email, password) => {
         try {
@@ -61,9 +63,19 @@ const AuthProvider = ({ children }) => {
             setUser(currentUser);
             if (currentUser) {
                 // get token and store client
+                const userInfo = { email: currentUser.email };
+                axiosPublic.post('jwt', userInfo)
+                    .then(res => {
+                        if (res.data.token) {
+                            localStorage.setItem('access-token', res.data.token);
+                        }
+
+                    })
+
             }
             else {
                 // TODO. remove token(if token stored in the client side:Local storage, caching, in memory )
+                localStorage.removeItem('access-token');
             }
             setLoading(false);
         });
